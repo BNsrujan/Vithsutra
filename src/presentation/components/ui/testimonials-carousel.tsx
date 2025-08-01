6161c013bc0743a56f82d552b4be1c7b26548813
BNsrujan <srujanbn12@gmail.com>
2025-08-01 12:48:16 +0530

    testimonial done

--- src/presentation/components/ui/testimonials-carousel.tsx
+++ src/presentation/components/ui/testimonials-carousel.tsx
@@ -1,65 +1,67 @@
 "use client";
 
 import Heading from "./heading";
-import {
-  TestimonialsCarouselProps,
-  Testimonial,
-} from "@/core/entities/testimonial";
+import { TestimonialsCarouselProps, Testimonial } from "@/core/entities/testimonial";
 import { cn } from "@/shared/lib/utils";
 import { Marquee } from "@/presentation/components/magicui/marquee.ui";
 import Image from "next/image";
 import { text } from "@/shared/lib/typography";
 
-export default function TestimonialsCarousel({
-  testimonials,
-}: TestimonialsCarouselProps) {
+export default function TestimonialsCarousel({ testimonials }: TestimonialsCarouselProps) {
   return (
     <div className="py-24 w-full flex justify-center">
       <section className="px-4 md:px-0 max-w-company-section-width mx-auto">
         <Heading heading="TESTIMONIALS" Display="What Our Clients Say" />
-        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+        <div className="relative flex w-full flex-col items-center pt-company-xl-48 justify-center overflow-hidden">
           <Marquee pauseOnHover className="[--duration:40s]">
-            {testimonials.map((testimonials) => (
-              <ReviewCard key={testimonials.id} {...testimonials} />
+            {testimonials.map((item) => (
+              <ReviewCard key={item.id} {...item} />
             ))}
           </Marquee>
 
-          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
-          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
+          {/* Gradient overlays for edges */}
+          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/12 bg-gradient-to-r from-background"></div>
+          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/12 bg-gradient-to-l from-background"></div>
         </div>
       </section>
     </div>
   );
 }
 
-const ReviewCard = (testimonials: Testimonial) => {
+const ReviewCard = ({ avatar, name, title, company, quote }: Testimonial) => {
   return (
     <figure
       className={cn(
-        "relative h-full md:w-[900px] cursor-pointer overflow-hidden rounded-xl border p-company-xl-48",
-      
+        "relative h-full md:w-[900px] cursor-pointer gap-company-md-16 overflow-hidden rounded-xl border border-company-primary-royalBlue p-company-xl-48"
       )}
     >
-      <div className="flex flex-row items-center  gap-2 w-full">
-        <Image
-          className="rounded-full w-16 h-16 object-cover grayscale-25"
-          width="32"
-          height="32"
-          alt=""
-          src={testimonials.avatar}
-        />
-        <div className="flex flex-col items-start">
-          <figcaption className={`${text.cardBodytext}  `}>
-            {testimonials.name}
-          </figcaption>
-         
-          <p className={`${text.cardBodytext} `}>
-          {testimonials.title} {testimonials.company}
-          </p>
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex gap-company-xs-8">
+          {avatar && (
+            <Image
+              className="rounded-full w-16 h-16 object-cover grayscale-25"
+              width={64}
+              height={64}
+              alt={name}
+              src={avatar}
+            />
+          )}
+          <div className="flex flex-col items-start gap-company-xs-8">
+            <figcaption className={text.DisplaySupportingtext}>{name}</figcaption>
+            <p className={text.Sectionbodytexts}>
+              {title} {company}
+            </p>
+          </div>
         </div>
-         <Image className=" place-self-end" src={"/favicon/favicon-96x96.png"} alt="vitsutra_image" width={80} height={80} />
+        <Image
+          src="/favicon/favicon-96x96.png"
+          alt="testimonial-logo"
+          width={80}
+          height={80}
+          quality={80}
+        />
       </div>
-      <blockquote className={`${text} `}>{testimonials.quote}</blockquote>
+      <blockquote>{quote}</blockquote>
     </figure>
   );
 };
